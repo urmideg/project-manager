1a24001e748f2964fe1b8dfae408526503a3c0be
Ivan Leonov <leonov.k13@yandex.ru>
2018-04-03 19:04:12 +0300

    #03.4. Метод index в контроллере DashboardController

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,7 +5,17 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+/**
+ * Панель администратора
+ */
 class DashboardController extends Controller
 {
-    //
+    /**
+     * Выдача главной станицы панели администратора
+     * @return представление admin.index
+     */
+    public function index()
+    {
+        return view('admin.index');
+    }
 }
