da691aee3796838922408b2628444b550ef30ad6
Ivan Leonov <leonov.k13@yandex.ru>
2018-04-03 21:57:25 +0300

    #07.3. Метод index для показа главной страницы

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index()
+    {
+        $works = Task::where('status', 'Реализация')->get();
+        $complites = Task::where('status', 'Завершена')->get();
+        return view('welcome', [
+            'works' => $works,
+            'complites' => $complites
+        ]);
+    }
 }
