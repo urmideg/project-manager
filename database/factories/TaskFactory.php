b7c95f859ded220257f67df59c511120694412fd
Ivan Leonov <leonov.k13@yandex.ru>
2018-04-03 21:09:10 +0300

    #06.3. Создание фабрики TaskFactory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    $users = \App\User::select('name')->where('role', 'junior')->get();
+    foreach ($users as $key => $user) {
+        $worker[] = $user->name;
+    }
+    return [
+        'name' => $faker->text(20),
+        'description' => $faker->text(50),
+        'worker' => $faker->randomElement($worker),
+        'status' => $faker->randomElement(['Реализация', 'Завершена']),
+    ];
+});
