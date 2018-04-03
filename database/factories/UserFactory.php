6177f128a07afee18a856f68dac39dad74016ef4
Ivan Leonov <leonov.k13@yandex.ru>
2018-04-03 19:41:32 +0300

    #04.2. Модификация фабрики UserFactory, добавление состояний

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,20 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => bcrypt('123456'),
         'remember_token' => str_random(10),
     ];
 });
+/**
+ * Состояние для учетной записи ведущего программиста
+ */
+$factory->state(App\User::class, 'senior', [
+     'email' => 'senior@test.ru',
+     'role' => 'senior',
+]);
+/**
+ * Состояние для учетной записи программистов
+ */
+$factory->state(App\User::class, 'junior', [
+     'role' => 'junior',
+]);
