45cff4c32405ec37f00bc71b7fde92df8363e146
Ivan Leonov <leonov.k13@yandex.ru>
2018-04-03 21:33:37 +0300

    #06.9. Представление для списка задач

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -34,6 +34,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                          <a class="nav-link" href="{{ route('admin.task.index') }}">Задачи</a>
+                        </li>
                         {{-- Пункт меню только для ведущего программиста --}}
                         @can ('senior')
                             <li class="nav-item">
---
+++ resources/views/admin/tasks/index.blade.php
@@ -0,0 +1,45 @@
+@extends('admin.layouts.app')
+
+@section('content')
+
+  <div class="container">
+    <div class="card">
+
+        <h2 class="card-header float-left">
+          Список задач
+        </h2>
+
+        <div class="card-body">
+          <table class="table table-responsiv table-striped table-bordered table-sm">
+            <thead>
+              <th scope="col">#</th>
+              <th scope="col">Название</th>
+              <th scope="col">Статус</th>
+              <th scope="col">Отвественный</th>
+              <th scope="col" class="text-right">Действия</th>
+            </thead>
+            <tbody>
+              @forelse ($tasks as $key => $task)
+                <tr>
+                  <th scope="row">{{++$key}}</th>
+                  <td>{{$task->name}}</td>
+                  <td>{{$task->status}}</td>
+                  <td>{{$task->worker}}</td>
+                  <td class="text-right">
+                  </td>
+                </tr>
+              @empty
+                <tr>
+                  <td colspan="5" class="text-center"><h2>Данные отсутствуют</h2></td>
+                </tr>
+              @endforelse
+            </tbody>
+          </table>
+          <ul class="pagination pull-right">
+            {{$tasks->links()}}
+          </ul>
+        </div>
+      </div>
+  </div>
+
+@endsection
